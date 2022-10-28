9750a3b2a93cbc8fc7f7a66884c60905399f719c
Stephen Adegbokun <obaadegbokun1@gmail.com>
2022-10-28 02:36:08 +0100

    Updating the app

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config['JSON_SORT_KEYS'] = False
+
+# defining data to return 
+data = {
+    "slackUsername" : "steefin",
+    "backend" : True,
+    "age" : 18,
+    "bio" : "Barca fan, AI enthusiast and a frequently intruigued guy. Also a UI student"
+    }
+
+@app.route('/api/info', methods=['GET'])
+def return_info():
+    if (request.method == 'GET'):
+        return jsonify(data)
+
+        
+app.run()
